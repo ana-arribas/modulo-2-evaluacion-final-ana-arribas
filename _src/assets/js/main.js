bea7db6b8635a4c460fcfea9d8149e4e6ea62f2e
Ana Arribas <arribas.ibd@gmail.com>
2019-12-03 18:35:37 +0100

    add dynamic remove buttons

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,6 +6,7 @@ const resultsList = document.querySelector('#results-list');
 const button = document.querySelector('#button');
 const urlBase = 'http://api.tvmaze.com/search/shows?q=';
 const favouritesList = document.querySelector('#favourites-list');
+let arrayFavourites = [];
 
 const init = () => {
     const myLocalStorage = localStorage.getItem("settingFavourites");
@@ -26,24 +27,29 @@ const searchHandler = () => {
 };
 
 const displayShows = arrayFromFetch => {
-    for (let show of arrayFromFetch) {
-        const elementLi = document.createElement('li');
-        const elementImg = document.createElement('img');
-        const elementSpan = document.createElement('span');
-        elementSpan.innerHTML = show.show.name;
-        if (show.show.image === null) {
-            elementImg.src = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+    if (input.value.length !== 0) {
+        for (let show of arrayFromFetch) {
+            const elementLi = document.createElement('li');
+            const elementImg = document.createElement('img');
+            const elementSpan = document.createElement('span');
+            elementSpan.innerHTML = show.show.name;
+            if (show.show.image === null) {
+                elementImg.src = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+            }
+            else {
+                elementImg.src = show.show.image.medium;
+            }
+            elementSpan.classList.add('dynamic-class-span');
+            elementImg.classList.add('dynamic-class-img');
+            elementLi.addEventListener('click', chooseFavourite);
+            elementLi.addEventListener('click', setShows);
+            elementLi.appendChild(elementSpan);
+            elementLi.appendChild(elementImg);
+            resultsList.appendChild(elementLi);
         }
-        else {
-            elementImg.src = show.show.image.medium;
-        }
-        elementSpan.classList.add('dynamic-class-span');
-        elementImg.classList.add('dynamic-class-img');
-        elementLi.addEventListener('click', chooseFavourite);
-        elementLi.addEventListener('click', setShows);
-        elementLi.appendChild(elementSpan);
-        elementLi.appendChild(elementImg);
-        resultsList.appendChild(elementLi);
+    }
+    else {
+        alert('introduce una serie');
     }
 };
 
@@ -62,7 +68,6 @@ const hidePreviousResults = () => {
     }
 };
 
-let arrayFavourites = [];
 const setShows = event => {
     const showTitle = event.currentTarget.querySelector('.dynamic-class-span');
     const showImage = event.currentTarget.querySelector('.dynamic-class-img');
@@ -82,12 +87,41 @@ const paintList = myArray => {
 };
 
 const PermanentList = clickedShow => {
+    const favouriteLi = document.createElement('li');
     const favouriteSpan = document.createElement('span');
     const favouriteImg = document.createElement('img');
+    const favouriteDelete = document.createElement('button');
     favouriteSpan.innerHTML = clickedShow.name;
     favouriteImg.src = clickedShow.img;
-    favouritesList.appendChild(favouriteSpan);
-    favouritesList.appendChild(favouriteImg);
+    favouriteDelete.innerHTML = 'Borrar';
+    favouriteDelete.classList.add('delete');
+    favouriteLi.appendChild(favouriteSpan); favouriteLi.appendChild(favouriteDelete);
+    favouriteLi.appendChild(favouriteImg);
+    favouritesList.appendChild(favouriteLi);
+    const allDeleteButtons = document.querySelectorAll('.delete');
+    for (let eachDeleteButton of allDeleteButtons) {
+        eachDeleteButton.addEventListener('click', deleteFavourite);
+    }
+};
+
+const deleteFavourite = event => {
+    const myLocalStorage = localStorage.getItem("settingFavourites");
+    event.target.closest('li').remove();
+
+    arrayFavourites = JSON.parse(myLocalStorage);
+    // for (let object of arrayFavourites) {
+    const elementSelected = event.target.parentElement.innerHTML;
+    const indexOfElement = arrayFavourites.indexOf(elementSelected);
+
+    console.log(indexOfElement);
+    // }
+
+    // const elementSelected = event.target.parentElement.innerHTML;
+    // const indexOfElement = arrayFavourites.indexOf(singleObject);
+
+    // console.log(indexOfElement);
+
+
 };
 
 button.addEventListener('click', hidePreviousResults);
