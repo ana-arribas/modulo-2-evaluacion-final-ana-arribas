22987ba1e6972b4f5ab1e5bb2de4683ddab6ca03
Ana Arribas <arribas.ibd@gmail.com>
2019-12-03 09:27:56 +0100

    change to arrow functions

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,37 +2,35 @@
 
 const form = document.querySelector('#form');
 const input = document.querySelector('#input');
-const resultslist = document.querySelector('#resultslist');
+const resultsList = document.querySelector('#results-list');
 const button = document.querySelector('#button');
 const urlBase = 'http://api.tvmaze.com/search/shows?q=';
-const container = document.querySelector('#container');
-const favouritesList = document.querySelector('#favouriteslist');
+const container = document.querySelector('#results-container');
+const favouritesList = document.querySelector('#favourites-list');
 
-function init() {
+const init = () => {
     const myLocalStorage = localStorage.getItem("settingFavourites");
     if (myLocalStorage !== null) {
         favouritesArray = JSON.parse(myLocalStorage);
     }
     paintList(favouritesArray);
-}
+};
 
-function searchHandler() {
+const searchHandler = () => {
     const inputValue = input.value.toLowerCase();
     fetch(urlBase + inputValue)
         .then(response => response.json())
         .then(data => displayShows(data))
         .catch(function (error) {
-            console.log("Error al traer los datos del servidor", error);
+            console.log("No se han podido cargar los datos", error);
         });
 };
 
 const displayShows = arrayFromFetch => {
-
     for (let show of arrayFromFetch) {
         const elementLi = document.createElement('li');
         const elementImg = document.createElement('img');
         const elementSpan = document.createElement('span');
-
         elementSpan.innerHTML = show.show.name;
         if (show.show.image === null) {
             elementImg.src = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
@@ -40,8 +38,6 @@ const displayShows = arrayFromFetch => {
         else {
             elementImg.src = show.show.image.medium;
         }
-
-
         elementLi.classList.add('classli');
         elementSpan.classList.add('classspan');
         elementImg.classList.add('classimg');
@@ -49,58 +45,48 @@ const displayShows = arrayFromFetch => {
         elementLi.addEventListener('click', setShows);
         elementLi.appendChild(elementSpan);
         elementLi.appendChild(elementImg);
-        resultslist.appendChild(elementLi);
-        container.appendChild(resultslist);
+        resultsList.appendChild(elementLi);
+        container.appendChild(resultsList);
     }
-}
+};
 
-function chooseFavourite(event) {
+const chooseFavourite = event => {
     event.currentTarget.classList.toggle('selected');
-    // setSelectedElements(event);
-}
+};
 
-function introForSearch(event) {
+const introForSearch = event => {
     event.preventDefault();
     searchHandler();
-}
-
-function hidePreviousResults() {
-    if (container !== null) {
-        const dinamicClass = document.querySelectorAll('.classli');
-        for (let littleClass of dinamicClass)
-            littleClass.classList.add('hidden');
-    }
-}
-button.addEventListener('click', hidePreviousResults);
+};
 
 let favouritesArray = [];
-
-function setShows(event) {
+const setShows = event => {
     const showTitle = event.currentTarget.querySelector('.classspan');
     const showImage = event.currentTarget.querySelector('.classimg');
     let selectedShows = {
         name: showTitle.innerHTML,
         img: showImage.src
-    }
+    };
     favouritesArray.push(selectedShows);
     localStorage.setItem('settingFavourites', JSON.stringify(favouritesArray));
-
     addNewVisited(selectedShows);
-}
+};
 
-const paintList = (array) => {
+const paintList = array => {
     for (let item of array) {
         addNewVisited(item)
     }
-}
+};
 
-const addNewVisited = (obj) => {
-    favouritesList.innerHTML += `<span>${obj.name}</span><img src=${obj.img}>`
-}
+const addNewVisited = clickedShow => {
+    const favouriteSpan = document.createElement('span');
+    const favouriteImg = document.createElement('img');
+    favouriteSpan.innerHTML = clickedShow.name;
+    favouriteImg.src = clickedShow.img;
+    favouritesList.appendChild(favouriteSpan);
+    favouritesList.appendChild(favouriteImg);
+};
 
-
-
-
-form.addEventListener('submit', introForSearch);
 button.addEventListener('click', searchHandler);
+form.addEventListener('submit', introForSearch);
 window.addEventListener('load', init);
